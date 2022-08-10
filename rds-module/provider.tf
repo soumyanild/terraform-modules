4be92e0d6e453ca686ced4dbfe8e1fbdeec9981d
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-10 16:35:43 +0530

    Update provider.tf

--- rds-module/provider.tf
+++ rds-module/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAU4YRJTBSSYJQQP5U"
-  secret_key = "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"
+  access_key = "AKIAU4YRJTBSSO7BS2WL"
+  secret_key = "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"
 }
