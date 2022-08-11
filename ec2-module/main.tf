1b624d59fc2dd9f469f2bcccd25a13d76029de62
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-11 17:21:17 +0530

    Update main.tf

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "nilzmann37-s3-bucket"
+    key    = "terraform-project/ec2-module/terraform.tfstate"
+    region = "ap-south-1"
+    }
+}
+
 resource "aws_instance" "public_inst_1" {
   ami           = "ami-0c1a7f89451184c8b"
   instance_type = "t2.micro"
