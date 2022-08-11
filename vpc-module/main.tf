e60f3d58b3b80f5580dee890a5e2d0c5da88dcca
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-11 17:12:57 +0530

    Update main.tf

--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -1,3 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "soumyanil-bucket"
+    key    = "terraform-project/backend/terraform.tfstate"
+    region = "ap-south-1"
+    }
+}
 resource "aws_vpc" "module" {
   cidr_block       = "10.0.0.0/24"
   instance_tenancy = "default"
