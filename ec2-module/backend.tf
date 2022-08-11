b69eeb8bf45f20af257e8ee5a607539fc2189d57
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-11 13:41:33 +0530

    Update backend.tf

--- ec2-module/backend.tf
+++ ec2-module/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "soumyanil-bucket"
+    bucket = "terraform-soumyanild"
     key    = "terraform-project/ec2-module/terraform.tfstate"
     region = "ap-south-1"
 
