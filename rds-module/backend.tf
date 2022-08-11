ffd5afbda4ec14fea572651ac496d13fd23dea25
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-11 12:28:36 +0530

    Create backend.tf

---
+++ rds-module/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-soumyanild"
+    key    = "terraform-project/rds-module/terraform.tfstate"
+    region = "ap-south-1"
+
+  }
+}  access_key = "AKIAU4YRJTBSSO7BS2WL"
+4
+  secret_key = "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"
