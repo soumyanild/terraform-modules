ec045722c895840e0677822c48875e0bab9a044b
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-11 17:15:39 +0530

    Update main.tf

--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "soumyanil-bucket"
+    bucket = "nilzmann37-s3-bucket"
     key    = "terraform-project/backend/terraform.tfstate"
     region = "ap-south-1"
     }
