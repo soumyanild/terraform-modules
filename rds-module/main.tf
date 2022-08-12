912929a9bc2e412e14848e401bc7fb2279c67307
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-12 13:46:14 +0530

    Update main.tf

--- rds-module/main.tf
+++ rds-module/main.tf
@@ -8,13 +8,3 @@ username             = "soumyanil"
 password             = "nilzyboy37"
 skip_final_snapshot  = false
 }
-resource "aws_db_instance" "rds_demo" {
-allocated_storage    = 10
-engine               = "mysql"
-engine_version       = "5.7"
-instance_class       = "db.t2.micro"
-identifier           = "rds-modules"
-username             = "soumyanil"
-password             = "nilzyboy37"
-skip_final_snapshot  = false
-}
