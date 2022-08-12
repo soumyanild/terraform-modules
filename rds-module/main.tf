67ba688e7f6379c72680d5c0648bd6a47d9d11d9
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-08-12 18:50:19 +0530

    Update main.tf

--- rds-module/main.tf
+++ rds-module/main.tf
@@ -1,3 +1,16 @@
+data "terraform_remote_state" "vpc" {
+    backend = "s3"
+    config {
+        bucket  = "terraform-soumyanild"
+        key     = "terraform-project/subnet-module/terraform.tfstate"
+        region  = "ap-south-1"
+    }
+}
+
+resource "aws_db_subnet_group" "db-subnet-group" {
+subnet_ids = ["${data.terraform_remote_state.vpc.dev_private_1_id}" , "${data.terraform_remote_state.vpc.dev_private_2_id}"]
+}
+
 resource "aws_db_instance" "private_rds" {
 allocated_storage    = 20
 engine               = "mysql"
@@ -10,8 +23,4 @@ password             = "code1234"
 publicly_accessible  = false
 skip_final_snapshot  = true
 db_subnet_group_name = "${aws_db_subnet_group.db-subnet-group.name}"
-}
-
-module "db_subnet_group" {
-  source = "terraform-modules/subnet-module/aws_db_subnet_group"
 }
